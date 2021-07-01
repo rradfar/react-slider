4e3174be857a419f01cdedca2fb42beda595bc67
Ramtin <31255934+rradfar@users.noreply.github.com>
2021-07-01 15:09:21 -0700

    Add the left & right buttons

--- src/Components/Slider/BtnSlider.js
+++ src/Components/Slider/BtnSlider.js
@@ -4,6 +4,13 @@ import leftArrow from './icons/left-arrow.svg';
 import rightArrow from './icons/right-arrow.svg';
 import './Slider.css';
 
-export default function BtnSlider() {
-  return <div>Buttons</div>;
+export default function BtnSlider({ direction, moveSlide }) {
+  return (
+    <button
+      className={direction === 'next' ? 'btn-slide next' : 'btn-slide prev'}
+      onClick={moveSlide}
+    >
+      <img src={direction === 'next' ? rightArrow : leftArrow} alt='' />
+    </button>
+  );
 }
--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -5,20 +5,35 @@ import dataSlider from './dataSlider';
 import './Slider.css';
 
 export default function Slider() {
+  const [slideIndex, setSlideIndex] = useState(1);
+  const nextSlide = () => {
+    slideIndex === dataSlider.length
+      ? setSlideIndex(1)
+      : setSlideIndex(slideIndex + 1);
+  };
+  const prevSlide = () => {
+    slideIndex === 1
+      ? setSlideIndex(dataSlider.length)
+      : setSlideIndex(slideIndex - 1);
+  };
+
   return (
     <div className='container-slider'>
       {dataSlider.map((obj, index) => {
         return (
-          <div className='slide'>
+          <div
+            className={slideIndex === index + 1 ? 'slide active-anim' : 'slide'}
+            key={obj.id}
+          >
             <img
               src={process.env.PUBLIC_URL + `/images/img${index + 1}.jpg`}
-              alt=''
+              alt={`${obj.title}`}
             />
           </div>
         );
       })}
-      <BtnSlider />
-      <BtnSlider />
+      <BtnSlider moveSlide={nextSlide} direction={'next'} />
+      <BtnSlider moveSlide={prevSlide} direction={'prev'} />
     </div>
   );
 }
